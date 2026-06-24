fb22192beb657897b0945cac6eedb0bdddb01b0f
kengo-harano-link <k-harano@link-si.co.jp>
2026-06-24 21:06:12 +0900

    Add files via upload

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -3,67 +3,107 @@
 
 <head>
    <meta charset="UTF-8">
-   <title>PHP課題</title>
+   <title>PHP基礎編</title>
 </head>
 
 <body>
    <p>
        <?php
        // クラスを定義する
-       class Food {
+       class Product {
            // プロパティを定義する
            public $name;
-           public $price;
 
-          // メソッドを定義する
-            public function show_price() {
-                echo $this->price;
-            }
-
-            // コンストラクタを定義する
-            public function __construct(string $name, int $price) {
+            // メソッドを定義する
+            public function set_name(string $name) {
                 $this->name = $name;
-                $this->price = $price;
             }
-
-        }
-
-        class Animal {
-           // プロパティを定義する
-           public $name;
-           public $height;
-           public $weight;
-
-          // メソッドを定義する
-            public function show_height() {
-                echo $this->height;
+            public function show_name() {
+                echo $this->name . '<br>';
             }
-
-            // コンストラクタを定義する
-            public function __construct(string $name, int $height, int $weight) {
-                $this->name = $name;
-                $this->height = $height;
-                $this->weight = $weight;
-            }
-
         }
 
         // インスタンス化する
-        $potato = new Food('potato', 250);
-        $dog = new Animal('dog', 60, 5000);
+        $coffee = new Product();
 
-        // インスタンス各プロパティの値を出力する
-        print_r($potato); 
-        echo '<br>';
-        print_r($dog);
-        echo '<br>';
+        // メソッドにアクセスして実行する
+        $coffee->set_name('コーヒー');
+        $coffee->show_name();
+
+       // インスタンス化する
+       $shampoo = new Product();
+
+       // プロパティにアクセスし、値を代入する
+       $shampoo->name = 'シャンプー';
 
        // プロパティにアクセスし、値を出力する
-       echo $potato->price.'<br>';
-       echo $dog->height;
+       echo $shampoo->name;
        ?>
    </p>
+    <p>
+        <?php
+        // クラスを定義する
+        class User {
+            // プロパティを定義する
+            private $name;
+            private $age;
+            private $gender;
 
+            // コンストラクタを定義する
+            public function __construct(string $name, int $age, string $gender) {
+                $this->name = $name;
+                $this->age = $age;
+                $this->gender = $gender;
+            }
+        }
+
+        // インスタンス化する
+        $user = new User('侍太郎', 36, '男性');
+
+        // インスタンス$userの各プロパティの値を出力する
+        print_r($user);
+        ?>
+    </p>
+    <p>
+      <?php
+        class Event
+        {
+            public string $name;
+            public string $date;
+            public string $location;
+
+            public function __construct(string $name, string $date, string $location)
+            {
+                $this->name = $name;
+                $this->date = $date;
+                $this->location = $location;
+            }
+
+            public function getDaysUntilEvent(): int
+            {
+                $today = new DateTime();
+                $eventDate = new DateTime($this->date);
+
+                return (int)$today->diff($eventDate)->format('%r%a');
+            }
+        }
+
+        // インスタンスを作成
+        $event = new Event(
+            'プログラミング初心者向けセミナー',
+            '2026-12-02',
+            '東京国際フォーラム'
+        );
+
+        // 各プロパティに直接アクセスして出力
+        echo 'イベント名：' . $event->name . PHP_EOL;
+        echo '開催日：' . $event->date . PHP_EOL;
+        echo '開催場所：' . $event->location . PHP_EOL;
+
+        // 開催日までの日数を出力
+        echo '開催日までの日数：' . $event->getDaysUntilEvent() . '日' . PHP_EOL;
+      ?>
+    </p>
 </body>
 
 </html>
